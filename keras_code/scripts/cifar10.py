b8e3bf3dd8713abdadc68c1b2cedf9952a487706
Brais Cancela Barizo <brais@wifi-27-18.inesctec.pt>
2019-03-25 14:27:40 +0000

    initial commit. Ready for cifar10

---
+++ keras_code/scripts/cifar10.py
@@ -0,0 +1,138 @@
+from keras_code.src.AdversarialRankN import AdversarialRankN
+from keras_code.scripts import small_networks, model_folder, info_folder
+from keras.datasets import cifar10
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import layers, models, regularizers, optimizers, losses
+from keras.callbacks import LearningRateScheduler
+import os
+import json
+from keras.utils import to_categorical
+
+
+finetuning = False
+optimizer = optimizers.adam(lr=1e-3)
+weights = 'imagenet'
+adam=True
+pooling = 'avg'
+epochs = 40 if finetuning else 130
+batch_size = 128
+Ns = [1, 3, 5]
+force_training = False
+adversarial_lr = 1e-2
+
+filename = 'cifar10_finetuning_' + str(finetuning) + '_adam_' + str(adam) + '_weights_' + str(weights) +'.json'
+
+generator = ImageDataGenerator(
+    width_shift_range=5. / 32,
+    height_shift_range=5. / 32,
+    fill_mode='reflect',
+    horizontal_flip=True
+)
+
+
+def scheduler(adam=True):
+    def sch(epoch):
+        if epoch < 40:
+            lr = .1
+        elif epoch < 80:
+            lr = .02
+        elif epoch < 115:
+            lr = .004
+        else:
+            lr = .0008
+        if adam:
+            lr /= 10.
+        return lr
+
+    return sch
+
+
+if __name__ == '__main__':
+    os.chdir('../../')
+
+    # x_train = x_train / 127.5 - 1.
+    # x_test = x_test / 127.5 - 1.
+
+    for network, preprocess in small_networks:
+
+        (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+        x_train = preprocess(x_train)
+        x_test = preprocess(x_test)
+
+        input_shape = (32, 32, 3)
+        classes = len(np.unique(y_train))
+
+        y_train = to_categorical(y_train, classes)
+        y_test = to_categorical(y_test, classes)
+
+        network_name = network.__name__
+        network_file = model_folder + 'cifar10_' + network_name + '_finetuning_' + str(finetuning) + \
+                       '_adam_' + str(adam) + '_weights_' + str(weights) + '.h5'
+
+        if os.path.isfile(network_file) and not force_training:
+            model = models.load_model(network_file)
+        else:
+            base_model = network(include_top=False, weights=weights, input_shape=input_shape, pooling=pooling)
+
+            if finetuning:
+                base_model.trainable = False
+            else:
+                base_model.trainable = True
+
+            x = base_model(base_model.input)
+
+            # x = layers.Flatten()(x)
+            #
+            x = layers.Dense(1024, use_bias=False, kernel_initializer='he_normal',
+                             kernel_regularizer=regularizers.l2(5e-4))(x)
+            x = layers.BatchNormalization(axis=-1, momentum=0.9, epsilon=1e-5, gamma_initializer='ones')(x)
+            x = layers.Activation('relu')(x)
+
+            x = layers.Dense(classes, use_bias=True, kernel_initializer='he_normal',
+                             kernel_regularizer=regularizers.l2(5e-4))(x)
+            x = layers.Activation('softmax')(x)
+
+            # this is the model we will train
+            model = models.Model(inputs=base_model.inputs, outputs=x)
+            model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['acc'])
+
+            callbacks = []
+            if not finetuning:
+                callbacks.append(
+                    LearningRateScheduler(scheduler(adam))
+                )
+
+            model.fit_generator(
+                generator.flow(x_train, y_train, batch_size=batch_size),
+                steps_per_epoch=x_train.shape[0] // batch_size, epochs=epochs,
+                callbacks=callbacks,
+                validation_data=(x_test, y_test),
+                validation_steps=x_test.shape[0] // batch_size,
+                verbose=1
+            )
+
+            if not os.path.isdir(model_folder):
+                os.makedirs(model_folder)
+
+            model.save(filepath=network_file)
+
+        adversarial_rank = AdversarialRankN(model=model)
+        scores = adversarial_rank.get_adversarial_scores(
+            x_test, y_test, Ns=Ns, batch_size=batch_size, lr=adversarial_lr
+        )
+
+        if not os.path.isdir(info_folder):
+            os.makedirs(info_folder)
+
+        try:
+            with open(info_folder + filename) as outfile:
+                info_data = json.load(outfile)
+        except:
+            info_data = {}
+
+        info_data[network_name] = scores
+
+        with open(info_folder + filename, 'w') as outfile:
+            json.dump(info_data, outfile)
---
+++ keras_code/src/AdversarialRankN.py
@@ -0,0 +1,103 @@
+from keras import backend as K, losses
+import numpy as np
+
+
+class AdversarialRankN:
+
+    def __init__(self, model):
+        self.model = model
+        self.adversarial_func = None
+
+    def build(self):
+        lp = K.learning_phase()
+        gradient = K.gradients(self.gain_function(
+            self.model.targets[0], self.model.outputs[0]), self.model.inputs)
+        self.adversarial_func = K.function([self.model.inputs[0], self.model.targets[0], lp], [gradient[0], self.model.outputs[0]])
+
+    def get_adversarial_scores(self, X, y, Ns, lr=1e-2, constraint=None, batch_size=10):
+        if self.adversarial_func is None:
+            self.build()
+
+        is_int = isinstance(Ns, int)
+        Ns = np.asarray([Ns]) if is_int else Ns
+        scores = {n: {'dist': np.zeros(len(X)), 'entropy': np.zeros(len(X))} for n in Ns}
+
+        active_indexes = np.arange(min(len(X), batch_size)).astype(int)
+        inactive_indexes = np.arange(min(len(X), batch_size), len(X)).astype(int)
+        y_argmin = np.asarray(y).argmin(axis=-1).reshape((-1, 1))
+
+        X_adversarial = X[active_indexes].copy()
+
+        batch_not_computed = np.ones((len(Ns), batch_size), dtype=bool)
+
+        cont = 0
+        while len(active_indexes):
+            if cont % 1000 == 0:
+                print('Remaining :', len(inactive_indexes) + len(active_indexes))
+
+            gradient, output = self.adversarial_func([X_adversarial, y[active_indexes], 0])
+
+            rank_output = (-1. * output).argsort(axis=-1)
+
+            pos_output = (y_argmin[active_indexes] == rank_output).argmax(axis=-1)
+
+            for i, n in enumerate(Ns):
+                completed = np.where((pos_output >= n) & batch_not_computed[i])[0]
+                if len(completed):
+                    pos = active_indexes[completed]
+                    scores[n]['dist'][pos] = self.compute_dist(X[pos], X_adversarial[completed])
+                    scores[n]['entropy'][pos] = self.compute_entropy(X[pos], X_adversarial[completed])
+                    batch_not_computed[i, completed] = False
+
+            incompleted = np.where(batch_not_computed.sum(axis=0) > 0)[0]
+
+            X_adversarial[incompleted] -= lr * gradient[incompleted] # / np.max(np.abs(gradient), axis=-1, keepdims=True)
+            if constraint is not None:
+                X_adversarial[incompleted] = constraint(X_adversarial[incompleted])
+
+            if len(incompleted) != batch_size:
+                active_indexes = active_indexes[incompleted]
+                batch_not_computed = batch_not_computed[:, incompleted]
+                X_adversarial = X_adversarial[incompleted]
+                nslots = min(len(inactive_indexes), batch_size - len(active_indexes))
+                if nslots:
+                    active_indexes = np.concatenate((active_indexes, inactive_indexes[:nslots]))
+                    batch_not_computed = np.concatenate(
+                        (batch_not_computed, np.ones((len(Ns), nslots), dtype=bool)), axis=1
+                    )
+                    X_adversarial = np.concatenate((X_adversarial, X[inactive_indexes[:nslots]].copy()), axis=0)
+                    inactive_indexes = inactive_indexes[nslots:]
+            cont += 1
+        return scores[Ns[0]] if is_int else scores
+
+    @classmethod
+    def gain_function(cls, y_true, y_pred):
+        y_pred = cls.clip(K.epsilon(), 1. - K.epsilon())(y_pred)
+        return K.sum(y_true * K.log(1. - y_pred), axis=-1)
+
+    @staticmethod
+    def compute_dist(X, X_adversarial):
+        return np.linalg.norm((X - X_adversarial).reshape((-1, np.prod(X.shape[1:]))), axis=1)
+
+    @staticmethod
+    def compute_entropy(X, X_adversarial):
+        diff = np.square(X - X_adversarial).reshape((-1, np.prod(X.shape[1:])))
+        nfeats = diff.shape[-1]
+        diff /= np.maximum(1e-6, diff.sum(axis=1, keepdims=True))
+        diff = diff * np.log(diff)
+        diff[np.isinf(diff) | np.isnan(diff)] = 0.
+        return np.log(nfeats) - diff.sum(axis=1)
+
+    @staticmethod
+    def clip(min_value, max_value):
+        @K.tf.custom_gradient
+        def clip_by_value(x):
+            # x_clip = K.clip(x - bias, -2., 2.)
+            s = K.clip(x, min_value, max_value)
+
+            def grad(dy):
+                return dy
+
+            return s, grad
+
+        return clip_by_value
